b10dc409912b9c7bdd5436b2ab0630f3c7ab1a08
Daria Borisiak <daria.borisiak@gmail.com>
2022-08-29 23:45:18 +0200

    update UI, fix some bugs

--- devtools_extension/js/devtools.js
+++ devtools_extension/js/devtools.js
@@ -2,10 +2,10 @@ const someFunc = function (element) {
     const isSelectorUnique = (selector) => document.querySelectorAll(selector).length === 1
 
     const getElementAttributes = (element) => ({
-        id: element.id,
-        dataTestId: element.getAttribute('data-testid'),
-        dataLocator: element.getAttribute('data-locator'),
-        tag: element.tagName.toLowerCase(),
+        id: element && element.id,
+        dataTestId: element && element.getAttribute('data-testid'),
+        dataLocator: element && element.getAttribute('data-locator'),
+        tag: element && element.tagName && element.tagName.toLowerCase(),
     })
 
     /**
@@ -17,10 +17,24 @@ const someFunc = function (element) {
         const getElementProperty = (element) => {
             return prop.includes('data')
                 ? element.getAttribute(prop)
-                : element.tagName.toLowerCase()
+                : element.tagName && element.tagName.toLowerCase()
         }
 
         let property = getElementProperty(node)
+
+        const siblings = node.parentNode.childNodes
+
+        let twinsCount = 0
+
+        for (const sibling of siblings) {
+            if (getElementProperty(sibling) === property) {
+                twinsCount++
+            }
+        }
+
+        if (twinsCount === 1) {
+            return null
+        }
 
         while(node.previousSibling) {
             node = node.previousSibling;
@@ -52,13 +66,11 @@ const someFunc = function (element) {
             }
         } else {
             return {
-                selector: tag.toLowerCase(),
+                selector: tag,
                 prop: 'tag',
             }
         }
     }
-
-    let result = {}
 
     let {selector: _selector, prop} = getElementSelector(element)
 
@@ -66,6 +78,19 @@ const someFunc = function (element) {
         return {
             unique: true,
             selector: _selector,
+        }
+    }
+
+    const index = getIndex(element, prop)
+
+    if (index !== null) {
+        _selector = `${_selector}:nth-of-type(${index})`
+
+        if (isSelectorUnique(_selector)) {
+            return {
+                unique: true,
+                selector: _selector,
+            }
         }
     }
 
@@ -82,29 +107,29 @@ const someFunc = function (element) {
             }
         }
 
-        const index = getIndex(parent, prop)
+        const index = getIndex(parent, parentProp)
 
-        selectorCandidate = `${parentSelector}:nth-of-type(${index}) ${_selector}`
+        if (index !== null) {
+            selectorCandidate = `${parentSelector}:nth-of-type(${index}) ${_selector}`
 
-        if (isSelectorUnique(selectorCandidate)) {
-            return {
-                unique: true,
-                selector: selectorCandidate
+            if (isSelectorUnique(selectorCandidate)) {
+                return {
+                    unique: true,
+                    selector: selectorCandidate
+                }
             }
         }
 
         _selector = selectorCandidate
         parent = parent.parentNode
 
-        if (!parent.parentNode) {
+        if (!parent || parent.tagName === 'BODY') {
             return {
                 unique: false,
                 selector: _selector,
             }
         }
     }
-
-    console.log({result})
 
     return {
         unique: true,
@@ -113,7 +138,6 @@ const someFunc = function (element) {
 };
 
 function handleSelectedElement() {
-    console.log('selection changed!!!')
     chrome.devtools.inspectedWindow.eval(`(${someFunc})($0)`, (res, err) => {
         if (err) {
             console.warn('Error', err);
@@ -121,19 +145,27 @@ function handleSelectedElement() {
 
             const { selector, unique } = res
 
+            const welcomeMessage = document.getElementById('welcome-message')
+            if (welcomeMessage) {
+                welcomeMessage.remove()
+            }
+
             const div = document.getElementById('selector')
 
             if (div) {
-                div.innerText = selector
+                div.innerHTML = `<div>\'${selector.split(' ').map(node => `<span>${node}</span>`).join('&#32;')}\'</div>`;
                 const isUniqueDiv = document.createElement('div')
-                isUniqueDiv.innerText = `Is unique: ${unique}`
+                isUniqueDiv.classList.add('is-unique-container')
+                isUniqueDiv.innerHTML = unique
+                    ? `<div class="success-message">This selector is unique</div>`
+                    : `<div class="fail-message"><span>${String.fromCodePoint(0x26A0)}</span>&nbsp;This selector is <strong>not</strong> unique</div>`
                 div.appendChild(isUniqueDiv)
             }
         }
     });
 }
 
-chrome.devtools.panels.elements.createSidebarPane("DASHAS SIDEBAR",
+chrome.devtools.panels.elements.createSidebarPane("Selector Wizard",
     function(sidebar) {
         sidebar.setPage("html/Sidebar.html");
         sidebar.setHeight("8ex");
